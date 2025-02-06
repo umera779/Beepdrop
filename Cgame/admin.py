c8bbe979e37fb7e49ecb5c452234d9f4a1b0f9f9
umera779 <109315806+umera779@users.noreply.github.com>
2025-02-06 21:12:55 +0100

    Update admin.py

--- Cgame/admin.py
+++ Cgame/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from . models import Counter, Mining, TaskList, Boost, Level, CustomUser, ButtonState
+from . models import Counter, TaskList, CustomUser, ButtonState,Referral
 admin.site.register(Counter)
-admin.site.register(Mining)
 admin.site.register(CustomUser)
-admin.site.register( Level)
 admin.site.register(ButtonState)
+admin.site.register(Referral)
 
 # Register your models here.
 class TaskListAdmin(admin.ModelAdmin):
@@ -18,15 +17,5 @@ class TaskListAdmin(admin.ModelAdmin):
         display_assigned_users.short_description = 'Assigned Users'
 
 
-class BoostListAdmin(admin.ModelAdmin):
-    list_display = ('boost_name', 'boost_value', 'display_boosting_users')
-    search_fields = ('boost_name',)
-    list_filter = ('boost_value',)
-    filter_horizontal = ('assigned_users',)
-
-    def display_boosting_users(self, obj):
-        return ", ".join([user.username for user in obj.assigned_users.all()])
-        display_boosting_users.short_description = "Assigned Users"
 
 admin.site.register(TaskList, TaskListAdmin)
-admin.site.register(Boost, BoostListAdmin)
